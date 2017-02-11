d5c3c7eb7c0b12b8f9c5875fbb494b43a25f7ebe
a-tarr <anthonyktarr@gmail.com>
2017-02-12 00:44:35 +1100

    removed comments

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './Nav';
 import App from './App';
 
-//
 export default class Main extends Component {
   render() {
     return (
@@ -13,7 +12,6 @@ export default class Main extends Component {
           <Route exact path="/" component={() =><App store={this.props.store}/>} />
           <Route path="/about" component={() =><div>put stuff here</div>} />
           <Route path="/contact" component={() =><div>putcontact here</div>} />
-          {/* <Route path="/asd" render={() => <div>asd</div>} /> */}
         </div>
       </Router>
     );
