2a345bb1d3da8c33ce5e147f0800d8cd4aa685dc
a-tarr <anthonyktarr@gmail.com>
2017-02-12 11:28:22 +1100

    add google analytics

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './Nav';
 import App from './App';
 import About from './About';
+const ReactGA = require('react-ga');
+
+ReactGA.initialize('UA-91813265-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 export default class Main extends Component {
   render() {
     return (
-      <Router>
+      <Router onUpdate={logPageView} >
         <div>
           <Nav />
           <Route exact path="/" component={() =><App store={this.props.store}/>} />
